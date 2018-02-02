1c775dc9b073f329601a9eb24e00bcfcef091d1a
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2018-02-02 18:53:45 +0200

    Service provider with default config added.

---
+++ src/ServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Illuminated\Wikipedia;
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    public function register()
+    {
+        $this->mergeConfigFrom($this->getConfigPath(), 'wikipedia-grabber');
+    }
+
+    public function boot()
+    {
+        $this->publishes([
+            $this->getConfigPath() => config_path('wikipedia-grabber.php')
+        ], 'config');
+    }
+
+    private function getConfigPath()
+    {
+        return __DIR__ . '/../config/wikipedia-grabber.php';
+    }
+}
