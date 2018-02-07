539a090ab55df719abed580958fc532ef3aa4923
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2018-02-07 19:42:32 +0200

    Exlimit added.

--- src/Grabber/PageGrabbing.php
+++ src/Grabber/PageGrabbing.php
@@ -17,9 +17,10 @@ trait PageGrabbing
             'query' => array_merge([
                 'action' => 'query',
                 'format' => 'json',
-                'formatversion' => 2,
+                'formatversion' => 2, // https://www.mediawiki.org/wiki/API:Query#Getting_a_list_of_page_IDs
                 'redirects' => true,
-                'prop' => 'extracts',
+                'prop' => 'extracts', // https://en.wikipedia.org/w/api.php?action=help&modules=query%2Bextracts
+                'exlimit' => 1,
             ], $this->composeTargetParams($title)),
         ];
     }
