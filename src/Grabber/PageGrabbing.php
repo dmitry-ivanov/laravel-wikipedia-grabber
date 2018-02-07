85bd34f9de607f986ad521f8412ef230d3211e3a
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2018-02-07 19:27:29 +0200

    Page params composed.

--- src/Grabber/PageGrabbing.php
+++ src/Grabber/PageGrabbing.php
@@ -6,8 +6,21 @@ trait PageGrabbing
 {
     public function page($title)
     {
-        $params = $this->composeTargetParams($title);
+        $params = $this->composePageParams($title);
 
         dd($params);
+    }
+
+    protected function composePageParams($title)
+    {
+        return [
+            'query' => array_merge([
+                'action' => 'query',
+                'format' => 'json',
+                'formatversion' => 2,
+                'redirects' => true,
+                'prop' => 'extracts',
+            ], $this->composeTargetParams($title)),
+        ];
     }
 }
