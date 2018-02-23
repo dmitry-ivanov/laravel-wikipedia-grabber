19f000ab64bf2782b804413ad1bfd01d7c1795be
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2018-02-23 19:31:16 +0200

    Singular entity base class added.

--- src/Grabber/Target/Page.php
+++ src/Grabber/Target/Page.php
@@ -2,12 +2,8 @@
 
 namespace Illuminated\Wikipedia\Grabber\Target;
 
-use Illuminated\Wikipedia\Grabber\Parser;
-
-class Page extends Entity
+class Page extends SingularEntity
 {
-    protected $response;
-
     protected function grab()
     {
         $fullResponse = json_decode(
@@ -39,81 +35,5 @@ class Page extends Entity
                 'ppprop' => 'disambiguation',
             ], $this->targetParams()),
         ];
-    }
-
-    public function isSuccess()
-    {
-        return !$this->isMissing() && !$this->isInvalid();
-    }
-
-    public function isDisambiguation()
-    {
-        return !empty($this->response['pageprops']) && isset($this->response['pageprops']['disambiguation']);
-    }
-
-    /**
-     * @see https://www.mediawiki.org/wiki/API:Query#Missing_and_invalid_titles
-     */
-    public function isMissing()
-    {
-        return !empty($this->response['missing']);
-    }
-
-    /**
-     * @see https://www.mediawiki.org/wiki/API:Query#Missing_and_invalid_titles
-     */
-    public function isInvalid()
-    {
-        return !empty($this->response['invalid']);
-    }
-
-    public function getId()
-    {
-        if (!$this->isSuccess()) {
-            return null;
-        }
-
-        return $this->response['pageid'];
-    }
-
-    public function getTitle()
-    {
-        if (!$this->isSuccess()) {
-            return null;
-        }
-
-        return $this->response['title'];
-    }
-
-    public function getBody()
-    {
-        if ($this->isMissing()) {
-            return $this->getMissingBody();
-        }
-
-        if ($this->isInvalid()) {
-            return $this->getInvalidBody();
-        }
-
-        return (new Parser($this->response['extract'], $this->format))->parse();
-    }
-
-    private function getMissingBody()
-    {
-        return "The page `{$this->target}` does not exist.";
-    }
-
-    private function getInvalidBody()
-    {
-        $reason = !empty($this->response['invalidreason'])
-            ? "\n{$this->response['invalidreason']}"
-            : '';
-
-        return "The page `{$this->target}` is invalid.{$reason}";
-    }
-
-    public function __toString()
-    {
-        return $this->getBody();
     }
 }
---
+++ src/Grabber/Target/SingularEntity.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Illuminated\Wikipedia\Grabber\Target;
+
+use Illuminated\Wikipedia\Grabber\Parser;
+
+abstract class SingularEntity extends Entity
+{
+    protected $response;
+
+    public function isSuccess()
+    {
+        return !$this->isMissing() && !$this->isInvalid();
+    }
+
+    /**
+     * @see https://www.mediawiki.org/wiki/API:Query#Missing_and_invalid_titles
+     */
+    public function isMissing()
+    {
+        return !empty($this->response['missing']);
+    }
+
+    /**
+     * @see https://www.mediawiki.org/wiki/API:Query#Missing_and_invalid_titles
+     */
+    public function isInvalid()
+    {
+        return !empty($this->response['invalid']);
+    }
+
+    public function isDisambiguation()
+    {
+        return !empty($this->response['pageprops']) && isset($this->response['pageprops']['disambiguation']);
+    }
+
+    public function getId()
+    {
+        if (!$this->isSuccess()) {
+            return null;
+        }
+
+        return $this->response['pageid'];
+    }
+
+    public function getTitle()
+    {
+        if (!$this->isSuccess()) {
+            return null;
+        }
+
+        return $this->response['title'];
+    }
+
+    public function getBody()
+    {
+        if ($this->isMissing()) {
+            return $this->getMissingBody();
+        }
+
+        if ($this->isInvalid()) {
+            return $this->getInvalidBody();
+        }
+
+        return (new Parser($this->response['extract'], $this->format))->parse();
+    }
+
+    private function getMissingBody()
+    {
+        return "The page `{$this->target}` does not exist.";
+    }
+
+    private function getInvalidBody()
+    {
+        $reason = !empty($this->response['invalidreason'])
+            ? "\n{$this->response['invalidreason']}"
+            : '';
+
+        return "The page `{$this->target}` is invalid.{$reason}";
+    }
+
+    public function __toString()
+    {
+        return $this->getBody();
+    }
+}
