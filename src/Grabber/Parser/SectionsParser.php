96c4c181dcd2a602c950dfb40b5017797869d870
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2018-03-15 22:13:21 +0200

    Sections parsing logic initial.

--- src/Grabber/Parser/SectionsParser.php
+++ src/Grabber/Parser/SectionsParser.php
@@ -15,17 +15,28 @@ class SectionsParser
 
     public function sections()
     {
-        $sections = [[
+        $sections = collect([[
             'level' => 1,
             'title' => $this->title,
             'body' => null,
-        ]];
+        ]]);
 
-        foreach ($this->parse() as $item) {
-            dd($item);
+        $items = $this->parse();
+        foreach ($items as $item) {
+            if ($this->isTitle($item)) {
+                $sections->push([
+                    'level' => $this->level($item),
+                    'title' => $this->title($item),
+                    'body' => null,
+                ]);
+            } else {
+                $last = $sections->pop();
+                $last['body'] = trim($item);
+                $sections->push($last);
+            }
         }
 
-        dd($parts);
+        return $sections;
     }
 
     private function parse()
@@ -35,5 +46,36 @@ class SectionsParser
         $pattern = "/({$marker}{$whitespace}.*?{$whitespace}{$marker})/";
 
         return preg_split($pattern, $this->body, -1, PREG_SPLIT_DELIM_CAPTURE);
+    }
+
+    private function isTitle($subject)
+    {
+        $marker = '[=]{2,}';
+        $whitespace = '\s*';
+        $pattern = "/{$marker}{$whitespace}.*?{$whitespace}{$marker}/";
+
+        return preg_match($pattern, $subject);
+    }
+
+    private function level($subject)
+    {
+        $marker = '[=]{2,}';
+        $whitespace = '\s*';
+        $pattern = "/({$marker}){$whitespace}.*?{$whitespace}{$marker}/";
+
+        preg_match($pattern, $subject, $matches);
+
+        return strlen($matches[1]);
+    }
+
+    private function title($subject)
+    {
+        $marker = '[=]{2,}';
+        $whitespace = '\s*';
+        $pattern = "/{$marker}{$whitespace}(.*?){$whitespace}{$marker}/";
+
+        preg_match($pattern, $subject, $matches);
+
+        return $matches[1];
     }
 }
