daf6d2a9f8474cfcf493ea58359bff29783bd320
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2018-03-15 21:37:47 +0200

    Title passed to sections parser.

--- src/Grabber/Parser.php
+++ src/Grabber/Parser.php
@@ -6,16 +6,18 @@ use Illuminated\Wikipedia\Grabber\Parser\SectionsParser;
 
 class Parser
 {
+    protected $title;
     protected $body;
 
-    public function __construct($body)
+    public function __construct($title, $body)
     {
+        $this->title = $title;
         $this->body = $body;
     }
 
     public function parse($format)
     {
-        $sections = (new SectionsParser($this->body))->sections();
+        $sections = (new SectionsParser($this->title, $this->body))->sections();
 
         dd($sections);
 
