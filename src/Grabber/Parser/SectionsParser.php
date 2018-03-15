3440d846b1aa09ce0be8794ec356d92fd9c4502f
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2018-03-15 21:38:07 +0200

    Title passed to sections parser.

--- src/Grabber/Parser/SectionsParser.php
+++ src/Grabber/Parser/SectionsParser.php
@@ -4,15 +4,36 @@ namespace Illuminated\Wikipedia\Grabber\Parser;
 
 class SectionsParser
 {
+    protected $title;
     protected $body;
 
-    public function __construct($body)
+    public function __construct($title, $body)
     {
+        $this->title = $title;
         $this->body = $body;
     }
 
     public function sections()
     {
-        dd('sections', $this->body);
+        $sections = [[
+            'level' => 1,
+            'title' => $this->title,
+            'body' => null,
+        ]];
+
+        foreach ($this->parse() as $item) {
+            dd($item);
+        }
+
+        dd($parts);
+    }
+
+    private function parse()
+    {
+        $marker = '[=]{2,}';
+        $whitespace = '\s*';
+        $pattern = "/({$marker}{$whitespace}.*?{$whitespace}{$marker})/";
+
+        return preg_split($pattern, $this->body, -1, PREG_SPLIT_DELIM_CAPTURE);
     }
 }
