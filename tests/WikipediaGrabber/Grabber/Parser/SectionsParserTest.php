4c3bc0a0a9b70a85979f1cfed370452d0ce128df
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2018-03-16 15:45:38 +0200

    Wip.

--- tests/WikipediaGrabber/Grabber/Parser/SectionsParserTest.php
+++ tests/WikipediaGrabber/Grabber/Parser/SectionsParserTest.php
@@ -2,18 +2,21 @@
 
 namespace Illuminated\Wikipedia\WikipediaGrabber\Tests\Grabber\Parser;
 
+use Illuminate\Support\Collection;
 use Illuminated\Wikipedia\Grabber\Parser\SectionsParser;
 use Illuminated\Wikipedia\WikipediaGrabber\Tests\TestCase;
 
 class SectionsParserTest extends TestCase
 {
     /** @test */
-    public function it_()
+    public function it_parses_sections_from_extracted_body()
     {
-        $body = file_get_contents(__DIR__ . '/SectionsParserTest/body.txt');
+        $body = file_get_contents(__DIR__ . '/SectionsParserTest/body-1.txt');
+        $sections = (new SectionsParser('Page title', $body))->sections();
 
-        $sections = (new SectionsParser('Александр Сергеевич Пушкин', $body))->sections();
+        $this->assertInstanceOf(Collection::class, $sections);
 
-        dd($sections);
+        $expected = require_once __DIR__ . '/SectionsParserTest/sections-1.php';
+        $this->assertEquals($expected, $sections->toArray());
     }
 }
