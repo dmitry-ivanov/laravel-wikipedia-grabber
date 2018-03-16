9b152ce4192edb7ede7111978ab11446d13d1b21
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2018-03-16 18:10:47 +0200

    Formatter factory method added & tested.

--- src/Grabber/Formatter/Formatter.php
+++ src/Grabber/Formatter/Formatter.php
@@ -4,4 +4,15 @@ namespace Illuminated\Wikipedia\Grabber\Formatter;
 
 abstract class Formatter
 {
+    public static function factory($format)
+    {
+        switch ($format) {
+            case 'bulma':
+                return new BulmaFormatter;
+
+            case 'plain':
+            default:
+                return new PlainFormatter;
+        }
+    }
 }
---
+++ tests/WikipediaGrabber/Grabber/Formatter/FormatterTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Illuminated\Wikipedia\WikipediaGrabber\Tests\Grabber\Formatter;
+
+use Illuminated\Wikipedia\Grabber\Formatter\BulmaFormatter;
+use Illuminated\Wikipedia\Grabber\Formatter\Formatter;
+use Illuminated\Wikipedia\Grabber\Formatter\PlainFormatter;
+use Illuminated\Wikipedia\WikipediaGrabber\Tests\TestCase;
+
+class FormatterTest extends TestCase
+{
+    /** @test */
+    public function it_has_static_factory_method_which_returns_plain_formatter_by_default()
+    {
+        $this->assertInstanceOf(PlainFormatter::class, Formatter::factory('foobar'));
+    }
+
+    /** @test */
+    public function and_it_returns_plain_formatter_if_asked()
+    {
+        $this->assertInstanceOf(PlainFormatter::class, Formatter::factory('plain'));
+    }
+
+    /** @test */
+    public function and_it_returns_bulma_formatter_if_asked()
+    {
+        $this->assertInstanceOf(BulmaFormatter::class, Formatter::factory('bulma'));
+    }
+}
