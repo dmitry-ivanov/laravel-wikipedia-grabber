45e3e83a95b4781031cd3ed24715ec91e887b61b
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2018-03-20 21:52:19 +0200

    Additional level check added.

--- src/Grabber/Formatter/Formatter.php
+++ src/Grabber/Formatter/Formatter.php
@@ -20,6 +20,10 @@ abstract class Formatter
 
     protected function titleTag($level)
     {
+        if ($level < 1) {
+            $level = 1;
+        }
+
         if ($level > 6) {
             $level = 6;
         }
