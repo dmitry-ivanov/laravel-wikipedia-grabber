daab54af3446ae2d95bd17758f5e11d7b7b68c50
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2018-03-21 12:31:21 +0200

    Section tests added.

---
+++ tests/WikipediaGrabber/Grabber/Component/SectionTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Illuminated\Wikipedia\WikipediaGrabber\Tests\Grabber\Component;
+
+use Illuminated\Wikipedia\Grabber\Component\Section;
+use Illuminated\Wikipedia\WikipediaGrabber\Tests\TestCase;
+
+class SectionTest extends TestCase
+{
+    /** @test */
+    public function it_makes_no_transformations_with_passed_title()
+    {
+        $section = new Section('  Title FOO  ', 'Body', 1);
+        $this->assertEquals('  Title FOO  ', $section->getTitle());
+    }
+
+    /** @test */
+    public function it_trims_passed_body()
+    {
+        $section = new Section('Title', '  Body FOO  ', 1);
+        $this->assertEquals('Body FOO', $section->getBody());
+    }
+
+    /** @test */
+    public function it_converts_passed_level_to_int()
+    {
+        $section = new Section('Title', 'Body', '7');
+        $this->assertSame(7, $section->getLevel());
+    }
+
+    /** @test */
+    public function and_if_level_is_less_than_1_then_it_would_be_1()
+    {
+        $section = new Section('Title', 'Body', 0);
+        $this->assertEquals(1, $section->getLevel());
+    }
+
+    /** @test */
+    public function it_has_get_html_level_method()
+    {
+        $section = new Section('Title', 'Body', 3);
+        $this->assertEquals(3, $section->getHtmlLevel());
+    }
+
+    /** @test */
+    public function which_will_return_6_if_level_is_greater_than_6()
+    {
+        $section = new Section('Title', 'Body', 7);
+        $this->assertEquals(6, $section->getHtmlLevel());
+    }
+}
