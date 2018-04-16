f22bc5a589f97a3fa77c8ad8978d9440ad7da210
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2018-04-16 16:23:43 +0300

    Test duplicates removed - same tests are on Wikipedia class.

--- tests/WikipediaGrabber/MediaWikiTest.php
+++ tests/WikipediaGrabber/MediaWikiTest.php
@@ -29,118 +29,17 @@ class MediaWikiTest extends TestCase
         );
     }
 
-    /** @test */
-    public function page_can_be_retrieved_by_title()
+    /**
+     * @test
+     * @runInSeparateProcess
+     * @preserveGlobalState disabled
+     */
+    public function it_returns_the_same_page_object_as_wikipedia_class_with_the_same_functionality()
     {
+        $this->mockWikipediaQuery();
+
         $page = (new MediaWiki('https://ru.wikipedia.org/w/api.php'))->page('Пушкин');
 
         $this->assertInstanceOf(Page::class, $page);
-        $this->assertTrue($page->isSuccess());
-        $this->assertFalse($page->isDisambiguation());
-        $this->assertEquals(537, $page->getId());
-        $this->assertEquals('Пушкин, Александр Сергеевич', $page->getTitle());
-    }
-
-    /** @test */
-    public function or_page_can_be_retrieved_by_id_if_integer_passed()
-    {
-        $page = (new MediaWiki('https://ru.wikipedia.org/w/api.php'))->page(537);
-
-        $this->assertTrue($page->isSuccess());
-        $this->assertFalse($page->isDisambiguation());
-        $this->assertEquals(537, $page->getId());
-        $this->assertEquals('Пушкин, Александр Сергеевич', $page->getTitle());
-    }
-
-    /** @test */
-    public function some_pages_can_be_marked_as_missed()
-    {
-        $page = (new MediaWiki('https://en.wikipedia.org/w/api.php'))->page('Fake-Unexisting-Page');
-
-        $this->assertTrue($page->isMissing());
-        $this->assertFalse($page->isSuccess());
-        $this->assertFalse($page->isDisambiguation());
-        $this->assertFalse($page->isInvalid());
-        $this->assertNull($page->getId());
-        $this->assertNull($page->getTitle());
-        $this->assertEquals('The page `Fake-Unexisting-Page` does not exist.', $page);
-        $this->assertEquals('The page `Fake-Unexisting-Page` does not exist.', $page->getBody());
-    }
-
-    /** @test */
-    public function some_pages_can_be_marked_as_invalid()
-    {
-        $page = (new MediaWiki('https://en.wikipedia.org/w/api.php'))->page('Talk:');
-
-        $this->assertTrue($page->isInvalid());
-        $this->assertFalse($page->isSuccess());
-        $this->assertFalse($page->isDisambiguation());
-        $this->assertFalse($page->isMissing());
-        $this->assertNull($page->getId());
-        $this->assertNull($page->getTitle());
-        $this->assertEquals(
-            "The page `Talk:` is invalid.\nThe requested page title is empty or contains only the name of a namespace.",
-            $page
-        );
-        $this->assertEquals(
-            "The page `Talk:` is invalid.\nThe requested page title is empty or contains only the name of a namespace.",
-            $page->getBody()
-        );
-    }
-
-    /** @test */
-    public function some_pages_can_be_marked_as_disambiguation()
-    {
-        $page = (new MediaWiki('https://en.wikipedia.org/w/api.php'))->page('David Taylor');
-
-        $this->assertTrue($page->isDisambiguation());
-        $this->assertTrue($page->isSuccess());
-        $this->assertFalse($page->isInvalid());
-        $this->assertFalse($page->isMissing());
-    }
-
-    /**
-     * @test
-     * @runInSeparateProcess
-     * @preserveGlobalState disabled
-     */
-    public function page_is_returned_in_specified_in_config_format_by_default()
-    {
-        $this->mockWikipediaQuery();
-
-        $parser = mock('overload:Illuminated\Wikipedia\Grabber\Parser\Parser');
-        $parser->expects()->parse('bulma');
-
-        (new MediaWiki('https://ru.wikipedia.org/w/api.php'))->page('Пушкин')->getBody();
-    }
-
-    /**
-     * @test
-     * @runInSeparateProcess
-     * @preserveGlobalState disabled
-     */
-    public function but_you_can_use_plain_helper_method_to_change_format_on_the_fly()
-    {
-        $this->mockWikipediaQuery();
-
-        $parser = mock('overload:Illuminated\Wikipedia\Grabber\Parser\Parser');
-        $parser->expects()->parse('plain');
-
-        (new MediaWiki('https://ru.wikipedia.org/w/api.php'))->page('Пушкин')->plain();
-    }
-
-    /**
-     * @test
-     * @runInSeparateProcess
-     * @preserveGlobalState disabled
-     */
-    public function there_is_also_bulma_helper_method_to_change_format_on_the_fly()
-    {
-        $this->mockWikipediaQuery();
-
-        $parser = mock('overload:Illuminated\Wikipedia\Grabber\Parser\Parser');
-        $parser->expects()->parse('bulma');
-
-        (new MediaWiki('https://ru.wikipedia.org/w/api.php'))->page('Пушкин')->bulma();
     }
 }
