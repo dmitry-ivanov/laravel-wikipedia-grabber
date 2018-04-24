f5b6af00ebfd4314b58fd3f7fb608a739bbe6a00
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2018-04-24 10:47:07 +0300

    Formatted.

--- tests/WikipediaGrabber/ServiceProviderTest.php
+++ tests/WikipediaGrabber/ServiceProviderTest.php
@@ -7,10 +7,8 @@ class ServiceProviderTest extends TestCase
     /** @test */
     public function it_merges_default_configuration_with_published_one()
     {
-        $config = config('wikipedia-grabber');
-
         $this->assertEquals(
-            $config['user_agent'],
+            config('wikipedia-grabber.user_agent'),
             'Laravel Wikipedia Grabber (https://github.com/dmitry-ivanov/laravel-wikipedia-grabber; dmitry.g.ivanov@gmail.com)'
         );
     }
