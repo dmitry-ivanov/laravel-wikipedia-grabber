4a5bff6c2f8edc5b741cadd36cb34f8e3009e941
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2018-04-26 14:12:39 +0300

    Test data added.

---
+++ tests/WikipediaGrabber/Grabber/Parser/ParserTest/main_image.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'original' => [
+        'source' => 'https://upload.wikimedia.org/wikipedia/commons/5/56/Portrait_of_Alexander_Pushkin_%28Orest_Kiprensky%2C_1827%29.PNG',
+        'width' => 1263,
+        'height' => 1472,
+    ],
+    'thumbnail' => [
+        'source' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Portrait_of_Alexander_Pushkin_%28Orest_Kiprensky%2C_1827%29.PNG/257px-Portrait_of_Alexander_Pushkin_%28Orest_Kiprensky%2C_1827%29.PNG',
+        'width' => 257,
+        'height' => 300,
+    ],
+];
