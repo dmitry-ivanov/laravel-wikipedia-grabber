3cf528ce40203a8b6d993691bade036e70f8bbcd
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2018-05-02 10:33:46 +0300

    Test data file renamed.

--- tests/WikipediaGrabber/Grabber/Parser/ParserTest.php
+++ tests/WikipediaGrabber/Grabber/Parser/ParserTest.php
@@ -10,8 +10,15 @@ class ParserTest extends TestCase
     /** @test */
     public function it_can_parse_body_for_plain_format_also_excluding_an_empty_and_boring_sections()
     {
-        $body = file_get_contents(__DIR__ . '/ParserTest/body.txt');
-        $parsed = (new Parser('Page title', $body))->parse('plain');
+        $body = file_get_contents(__DIR__ . '/ParserTest/extract.txt');
+        // $imagesResponseData = [
+        //     'wikitext' => file_get_contents(__DIR__ . '/ParserTest/wikitext.txt'),
+        //     'main_image' => require_once __DIR__ . '/ParserTest/main_image.php',
+        //     'images' => require_once __DIR__ . '/ParserTest/images.php',
+        // ];
+        $imagesResponseData = null;
+
+        $parsed = (new Parser('Page title', $body, $imagesResponseData))->parse('plain');
 
         $expects = file_get_contents(__DIR__ . '/ParserTest/plain.html');
         $this->assertEquals($expects, $parsed);
