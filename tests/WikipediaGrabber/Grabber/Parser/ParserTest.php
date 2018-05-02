824c9721a52b9ee492bd1c1da18cf54ca7a8147c
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2018-05-02 11:33:15 +0300

    Title changed.

--- tests/WikipediaGrabber/Grabber/Parser/ParserTest.php
+++ tests/WikipediaGrabber/Grabber/Parser/ParserTest.php
@@ -11,14 +11,13 @@ class ParserTest extends TestCase
     public function it_can_parse_body_for_plain_format_also_excluding_an_empty_and_boring_sections()
     {
         $body = file_get_contents(__DIR__ . '/ParserTest/extract.txt');
-        // $imagesResponseData = [
-        //     'wikitext' => file_get_contents(__DIR__ . '/ParserTest/wikitext.txt'),
-        //     'main_image' => require_once __DIR__ . '/ParserTest/main_image.php',
-        //     'images' => require_once __DIR__ . '/ParserTest/images.php',
-        // ];
-        $imagesResponseData = null;
+        $imagesResponseData = [
+            'wikitext' => file_get_contents(__DIR__ . '/ParserTest/wikitext.txt'),
+            'main_image' => require_once __DIR__ . '/ParserTest/main_image.php',
+            'images' => require_once __DIR__ . '/ParserTest/images.php',
+        ];
 
-        $parsed = (new Parser('Page title', $body, $imagesResponseData))->parse('plain');
+        $parsed = (new Parser('Александр Сергеевич Пушкин', $body, $imagesResponseData))->parse('plain');
 
         $expects = file_get_contents(__DIR__ . '/ParserTest/plain.html');
         $this->assertEquals($expects, $parsed);
