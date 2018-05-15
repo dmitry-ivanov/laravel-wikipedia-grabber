66886780d017214a58cced72078c4c767f6f7e80
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2018-05-15 17:19:43 +0300

    Image wikitext class - very initial.

---
+++ src/Grabber/Wikitext/ImageWikitext.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Illuminated\Wikipedia\Grabber\Wikitext;
+
+class ImageWikitext
+{
+    protected $body;
+
+    public function __construct($body)
+    {
+        $this->body = $body;
+    }
+
+    public function getPosition()
+    {
+        $parts = explode('|', trim($this->body, '[]'));
+        return in_array('left', $parts) ? 'left' : 'right';
+    }
+}
