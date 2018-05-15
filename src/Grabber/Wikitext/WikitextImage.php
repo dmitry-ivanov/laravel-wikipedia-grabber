95788648611c8fa4ce4fe30a541e1f212054d4b1
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2018-05-15 21:31:55 +0300

    WikitextImage started.

--- src/Grabber/Wikitext/WikitextImage.php
+++ src/Grabber/Wikitext/WikitextImage.php
@@ -4,9 +4,43 @@ namespace Illuminated\Wikipedia\Grabber\Wikitext;
 
 class WikitextImage extends Wikitext
 {
+    protected $position;
+
+    public function __construct($body)
+    {
+        parent::__construct($body);
+
+        $this->parse();
+    }
+
+    /**
+     * @see https://en.wikipedia.org/wiki/Wikipedia:Extended_image_syntax
+     */
+    protected function parse()
+    {
+        $body = $this->body;
+        $body = str_replace_first('[[', '', $body);
+        $body = str_replace_last(']]', '', $body);
+
+        $parts = explode('|', $body);
+        array_shift($parts);
+
+        foreach ($parts as $part) {
+            if ($this->isType($part)) {
+                continue;
+            }
+
+            dump($part);
+        }
+    }
+
+    protected function isType($string)
+    {
+        return starts_with($string, ['thumb', 'thumbnail', 'frame', 'framed', 'frameless']);
+    }
+
     public function getPosition()
     {
-        $parts = explode('|', trim($this->body, '[]'));
-        return in_array('left', $parts) ? 'left' : 'right';
+        return $this->position;
     }
 }
