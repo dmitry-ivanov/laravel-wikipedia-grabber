c742437dd7c5c1ae997f871a10091561840d4a83
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2018-06-20 14:09:53 +0300

    Multiline templates - tests added.

---
+++ tests/WikipediaGrabber/Grabber/Wikitext/Templates/MultilineTemplateTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Illuminated\Wikipedia\WikipediaGrabber\Tests\Grabber\Wikitext\Templates;
+
+use Illuminated\Wikipedia\Grabber\Component\Section;
+use Illuminated\Wikipedia\Grabber\Wikitext\Templates\MultilineTemplate;
+use Illuminated\Wikipedia\WikipediaGrabber\Tests\TestCase;
+
+class MultilineTemplateTest extends TestCase
+{
+    /** @test */
+    public function it_has_flatten_method_which_flattens_multiline_templates()
+    {
+        $body = trim(file_get_contents(__DIR__ . '/MultilineTemplateTest/body.txt'));
+        $section = new Section('Title', $body, 2);
+
+        $flatten = trim(file_get_contents(__DIR__ . '/MultilineTemplateTest/flatten.txt'));
+        $this->assertEquals($flatten, (new MultilineTemplate($section))->flatten());
+    }
+}
