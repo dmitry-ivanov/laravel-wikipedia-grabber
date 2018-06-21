0ca5027e3093b282d12e941d46587000d4d4af08
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2018-06-21 12:39:29 +0300

    StyleCI thanks.

--- tests/WikipediaGrabber/Grabber/Wikitext/Templates/MultipleImageTemplateTest.php
+++ tests/WikipediaGrabber/Grabber/Wikitext/Templates/MultipleImageTemplateTest.php
@@ -55,17 +55,17 @@ class MultipleImageTemplateTest extends TestCase
         $line = "{{Фотоколонка+|align=left|Mammillaria prolifera20100407 076.jpg|текст1=''[[Mammillaria prolifera]]''|Tussilago farfara20100409 07.jpg|текст2=''[[Tussilago farfara]]''|Succisa pratensis20090811 088.jpg|текст3=''[[Succisa pratensis]]''}}";
 
         $this->assertEquals(
-            "{{Фотоколонка+|left|Mammillaria prolifera20100407 076.jpg|текст=Mammillaria prolifera|Tussilago farfara20100409 07.jpg|Succisa pratensis20090811 088.jpg}}",
+            '{{Фотоколонка+|left|Mammillaria prolifera20100407 076.jpg|текст=Mammillaria prolifera|Tussilago farfara20100409 07.jpg|Succisa pratensis20090811 088.jpg}}',
             (new MultipleImageTemplate($line))->extract('Mammillaria prolifera20100407 076.jpg')
         );
 
         $this->assertEquals(
-            "{{Фотоколонка+|left|Mammillaria prolifera20100407 076.jpg|Tussilago farfara20100409 07.jpg|текст=Tussilago farfara|Succisa pratensis20090811 088.jpg}}",
+            '{{Фотоколонка+|left|Mammillaria prolifera20100407 076.jpg|Tussilago farfara20100409 07.jpg|текст=Tussilago farfara|Succisa pratensis20090811 088.jpg}}',
             (new MultipleImageTemplate($line))->extract('Tussilago farfara20100409 07.jpg')
         );
 
         $this->assertEquals(
-            "{{Фотоколонка+|left|Mammillaria prolifera20100407 076.jpg|Tussilago farfara20100409 07.jpg|Succisa pratensis20090811 088.jpg|текст=Succisa pratensis}}",
+            '{{Фотоколонка+|left|Mammillaria prolifera20100407 076.jpg|Tussilago farfara20100409 07.jpg|Succisa pratensis20090811 088.jpg|текст=Succisa pratensis}}',
             (new MultipleImageTemplate($line))->extract('Succisa pratensis20090811 088.jpg')
         );
     }
@@ -76,17 +76,17 @@ class MultipleImageTemplateTest extends TestCase
         $line = "{{Listen|type=music|filename=Accordion chords-01.ogg|title=Accordion chords|description=Chords being played on an accordion|filename2=Moonlight.ogg|title2=''Moonlight Sonata''|description2=[[Ludwig van Beethoven|Beethoven]]'s [[Piano Sonata No. 14 (Beethoven)|Sonata in C-sharp minor]]|filename3=Brahms-waltz15.ogg|title3=Waltz No. 15|description3=From [[Sixteen Waltzes, Op. 39 (Brahms)|Sixteen Waltzes, Op. 39]] by [[Johannes Brahms|Brahms]]}}";
 
         $this->assertEquals(
-            "{{Listen|type=music|filename=Accordion chords-01.ogg|title=Accordion chords|description=Chords being played on an accordion}}",
+            '{{Listen|type=music|filename=Accordion chords-01.ogg|title=Accordion chords|description=Chords being played on an accordion}}',
             (new MultipleImageTemplate($line))->extract('Accordion chords-01.ogg')
         );
 
         $this->assertEquals(
-            "{{Listen|type=music|filename=Accordion chords-01.ogg|title=Accordion chords|description=Chords being played on an accordion|filename=Moonlight.ogg|title=Moonlight Sonata|description=Beethoven's Sonata in C-sharp minor}}",
+            '{{Listen|type=music|filename=Accordion chords-01.ogg|title=Accordion chords|description=Chords being played on an accordion|filename=Moonlight.ogg|title=Moonlight Sonata|description=Beethoven\'s Sonata in C-sharp minor}}',
             (new MultipleImageTemplate($line))->extract('Moonlight.ogg')
         );
 
         $this->assertEquals(
-            "{{Listen|type=music|filename=Accordion chords-01.ogg|title=Accordion chords|description=Chords being played on an accordion|filename=Brahms-waltz15.ogg|title=Waltz No. 15|description=From Sixteen Waltzes, Op. 39 by Brahms}}",
+            '{{Listen|type=music|filename=Accordion chords-01.ogg|title=Accordion chords|description=Chords being played on an accordion|filename=Brahms-waltz15.ogg|title=Waltz No. 15|description=From Sixteen Waltzes, Op. 39 by Brahms}}',
             (new MultipleImageTemplate($line))->extract('Brahms-waltz15.ogg')
         );
     }
