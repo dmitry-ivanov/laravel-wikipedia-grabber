5bb077e6026e5fcf44d1291b742788f4c96b41fe
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2018-06-26 15:05:57 +0300

    Gallery validator improved - types logic.

--- src/Grabber/Component/GalleryValidator.php
+++ src/Grabber/Component/GalleryValidator.php
@@ -10,11 +10,55 @@ class GalleryValidator
     {
         $validated = ['gallery' => $gallery, 'not_gallery' => collect()];
 
+        $byTypes = $this->byTypes($gallery);
+        $main = $this->getMainType($byTypes);
+
+        $validated['gallery'] = $byTypes[$main];
+        foreach ($byTypes as $type => $collection) {
+            if ($type != $main) {
+                $validated['not_gallery'] = $validated['not_gallery']->merge($collection);
+            }
+        }
+
         if ($validated['gallery']->count() < 4) {
             $validated['gallery'] = collect();
             $validated['not_gallery'] = $gallery;
         }
 
         return $validated;
+    }
+
+    protected function byTypes(Collection $images)
+    {
+        $result = ['video' => collect(), 'audio' => collect(), 'images' => collect()];
+
+        foreach ($images as $image) {
+            if ($image->isVideo()) {
+                $result['video']->push($image);
+                continue;
+            }
+
+            if ($image->isAudio()) {
+                $result['audio']->push($image);
+                continue;
+            }
+
+            $result['images']->push($image);
+        }
+
+        return $result;
+    }
+
+    protected function getMainType(array $byTypes)
+    {
+        $counts = collect($byTypes)->map(function (Collection $collection, $key) {
+            return ['key' => $key, 'count' => $collection->count()];
+        });
+
+        $max = $counts->max('count');
+
+        return $counts->first(function ($item) use ($max) {
+            return $item['count'] == $max;
+        })['key'];
     }
 }
