12381c6eaaef8ee9f9e95f384ccbf735d72beb19
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2018-06-26 21:54:08 +0300

    Multiline files logic added.

---
+++ src/Grabber/Wikitext/Templates/MultilineFile.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Illuminated\Wikipedia\Grabber\Wikitext\Templates;
+
+use Illuminated\Wikipedia\Grabber\Component\Section;
+use Illuminated\Wikipedia\Grabber\Wikitext\Wikitext;
+
+class MultilineFile
+{
+    public function flatten(Section $section)
+    {
+        $flatten = collect();
+
+        $body = str_replace('[[Файл:', '[[File:', $section->getBody());
+
+        $isFileOpened = false;
+        $lines = preg_split("/\r\n|\n|\r/", $body);
+        foreach ($lines as $line) {
+            if ($isFileOpened) {
+                $flatten->push($flatten->pop() . ' ' . $line);
+                $isFileOpened = !$this->isFileClosed($line);
+            } else {
+                $flatten->push($line);
+                $isFileOpened = $this->isFileOpened($line);
+            }
+        }
+
+        return $flatten->implode("\n");
+    }
+
+    protected function isFileOpened($line)
+    {
+        if (!str_contains($line, '[[File:')) {
+            return false;
+        }
+
+        $line = str_replace('[[File:', '/!! IWG_FILE !!/', $line);
+
+        return !$this->isFileClosed($line);
+    }
+
+    protected function isFileClosed($line)
+    {
+        $line = (new Wikitext($line))->plain();
+        return str_contains($line, ']]');
+    }
+}
