eb9ed3441f02d58fb8838074279735c18e51dbaf
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2018-06-26 22:01:36 +0300

    Multiline files logic - tests added.

---
+++ tests/WikipediaGrabber/Grabber/Wikitext/Templates/MultilineFileTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Illuminated\Wikipedia\WikipediaGrabber\Tests\Grabber\Wikitext\Templates;
+
+use Illuminated\Wikipedia\Grabber\Component\Section;
+use Illuminated\Wikipedia\Grabber\Wikitext\Templates\MultilineFile;
+use Illuminated\Wikipedia\WikipediaGrabber\Tests\TestCase;
+
+class MultilineFileTest extends TestCase
+{
+    /** @test */
+    public function it_has_flatten_method_which_flattens_multiline_files()
+    {
+        $body = trim(file_get_contents(__DIR__ . '/MultilineFileTest/body.txt'));
+        $section = new Section('Title', $body, 2);
+
+        $flatten = trim(file_get_contents(__DIR__ . '/MultilineFileTest/flatten.txt'));
+        $this->assertEquals($flatten, (new MultilineFile)->flatten($section));
+    }
+}
