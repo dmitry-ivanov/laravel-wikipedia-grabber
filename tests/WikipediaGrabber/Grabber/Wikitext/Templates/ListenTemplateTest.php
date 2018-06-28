0f385df0812d59c04e5aa7e4e55c96a234dc6611
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2018-06-28 17:52:38 +0300

    Listen template - tests added.

--- tests/WikipediaGrabber/Grabber/Wikitext/Templates/ListenTemplateTest.php
+++ tests/WikipediaGrabber/Grabber/Wikitext/Templates/ListenTemplateTest.php
@@ -10,7 +10,7 @@ class ListenTemplateTest extends TestCase
     /** @test */
     public function it_has_transform_method_which_works_if_there_is_no_title_and_description()
     {
-        $template = new ListenTemplate("{{Listen | type = music | filename = Accordion chords-01.ogg | footer = Some footer }}");
+        $template = new ListenTemplate('{{Listen | type = music | filename = Accordion chords-01.ogg | footer = Some footer }}');
 
         $this->assertEquals(
             $template->transform(),
@@ -21,7 +21,7 @@ class ListenTemplateTest extends TestCase
     /** @test */
     public function and_it_works_for_the_case_when_only_title_set()
     {
-        $template = new ListenTemplate("{{Listen | type = music | filename = Accordion chords-01.ogg | title = Accordion chords | footer = Some footer }}");
+        $template = new ListenTemplate('{{Listen | type = music | filename = Accordion chords-01.ogg | title = Accordion chords | footer = Some footer }}');
 
         $this->assertEquals(
             $template->transform(),
@@ -32,7 +32,7 @@ class ListenTemplateTest extends TestCase
     /** @test */
     public function and_it_works_for_the_case_when_only_description_set()
     {
-        $template = new ListenTemplate("{{Listen | type = music | filename = Accordion chords-01.ogg | description = Chords being played on an accordion | footer = Some footer }}");
+        $template = new ListenTemplate('{{Listen | type = music | filename = Accordion chords-01.ogg | description = Chords being played on an accordion | footer = Some footer }}');
 
         $this->assertEquals(
             $template->transform(),
@@ -43,7 +43,7 @@ class ListenTemplateTest extends TestCase
     /** @test */
     public function and_it_works_for_the_case_when_title_and_description_are_set()
     {
-        $template = new ListenTemplate("{{Listen | type = music | filename = Accordion chords-01.ogg | title = Accordion chords | description = Chords being played on an accordion | footer = Some footer }}");
+        $template = new ListenTemplate('{{Listen | type = music | filename = Accordion chords-01.ogg | title = Accordion chords | description = Chords being played on an accordion | footer = Some footer }}');
 
         $this->assertEquals(
             $template->transform(),
