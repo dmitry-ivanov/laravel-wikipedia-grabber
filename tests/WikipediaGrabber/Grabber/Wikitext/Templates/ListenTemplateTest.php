d35ee95be332f24da95d8ef816a8af012d9805f5
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2018-06-28 17:48:27 +0300

    Listen template - tests added.

--- tests/WikipediaGrabber/Grabber/Wikitext/Templates/ListenTemplateTest.php
+++ tests/WikipediaGrabber/Grabber/Wikitext/Templates/ListenTemplateTest.php
@@ -10,33 +10,33 @@ class ListenTemplateTest extends TestCase
     /** @test */
     public function it_has_transform_method_which_works_for_the_case_of_title()
     {
-        $template = new ListenTemplate("{{Listen | type = music | filename = Accordion chords-01.ogg | title = Accordion chords }}");
+        $template = new ListenTemplate("{{Listen | type = music | filename = Accordion chords-01.ogg | title = Accordion chords | footer = Some footer }}");
 
         $this->assertEquals(
             $template->transform(),
-            '{{Listen|type = music|filename = Accordion chords-01.ogg|title=Accordion chords}}'
+            '{{Listen|type = music|filename = Accordion chords-01.ogg|footer = Some footer|title=Accordion chords}}'
         );
     }
 
     /** @test */
     public function it_has_transform_method_which_works_for_the_case_of_description()
     {
-        $template = new ListenTemplate("{{Listen | type = music | filename = Accordion chords-01.ogg | description = Chords being played on an accordion }}");
+        $template = new ListenTemplate("{{Listen | type = music | filename = Accordion chords-01.ogg | description = Chords being played on an accordion | footer = Some footer }}");
 
         $this->assertEquals(
             $template->transform(),
-            '{{Listen|type = music|filename = Accordion chords-01.ogg|title=Chords being played on an accordion}}'
+            '{{Listen|type = music|filename = Accordion chords-01.ogg|footer = Some footer|title=Chords being played on an accordion}}'
         );
     }
 
     /** @test */
     public function it_has_transform_method_which_works_for_the_case_of_title_and_description()
     {
-        $template = new ListenTemplate("{{Listen | type = music | filename = Accordion chords-01.ogg | title = Accordion chords | description = Chords being played on an accordion }}");
+        $template = new ListenTemplate("{{Listen | type = music | filename = Accordion chords-01.ogg | title = Accordion chords | description = Chords being played on an accordion | footer = Some footer }}");
 
         $this->assertEquals(
             $template->transform(),
-            '{{Listen|type = music|filename = Accordion chords-01.ogg|title=Accordion chords - Chords being played on an accordion}}'
+            '{{Listen|type = music|filename = Accordion chords-01.ogg|footer = Some footer|title=Accordion chords - Chords being played on an accordion}}'
         );
     }
 }
