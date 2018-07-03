9e66a7cfd0775514031c0dc2accb665adf8a5ff7
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2018-07-03 13:10:35 +0300

    Formatted.

--- tests/WikipediaGrabber/WikipediaTest.php
+++ tests/WikipediaGrabber/WikipediaTest.php
@@ -79,14 +79,8 @@ class WikipediaTest extends TestCase
         $this->assertFalse($page->isMissing());
         $this->assertNull($page->getId());
         $this->assertNull($page->getTitle());
-        $this->assertEquals(
-            "The page `Talk:` is invalid.\nThe requested page title is empty or contains only the name of a namespace.",
-            $page
-        );
-        $this->assertEquals(
-            "The page `Talk:` is invalid.\nThe requested page title is empty or contains only the name of a namespace.",
-            $page->getBody()
-        );
+        $this->assertEquals("The page `Talk:` is invalid.\nThe requested page title is empty or contains only the name of a namespace.", $page);
+        $this->assertEquals("The page `Talk:` is invalid.\nThe requested page title is empty or contains only the name of a namespace.", $page->getBody());
     }
 
     /** @test */
