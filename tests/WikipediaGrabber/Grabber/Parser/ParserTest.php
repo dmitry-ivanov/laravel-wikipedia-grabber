bf7e67ddb36788a1d9212708fbe9331e0d633e05
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2018-07-09 19:49:05 +0300

    Bulma formatter - wip.

--- tests/WikipediaGrabber/Grabber/Parser/ParserTest.php
+++ tests/WikipediaGrabber/Grabber/Parser/ParserTest.php
@@ -22,4 +22,20 @@ class ParserTest extends TestCase
         $expects = file_get_contents(__DIR__ . '/ParserTest/plain.html');
         $this->assertEquals($expects, $parsed);
     }
+
+    /** @test */
+    public function it_can_parse_body_for_bulma_format_also_excluding_an_empty_and_boring_sections()
+    {
+        $body = file_get_contents(__DIR__ . '/ParserTest/extract.txt');
+        $imagesResponseData = [
+            'wikitext' => file_get_contents(__DIR__ . '/ParserTest/wikitext.txt'),
+            'main_image' => require_once __DIR__ . '/ParserTest/main_image.php',
+            'images' => require_once __DIR__ . '/ParserTest/images.php',
+        ];
+
+        $parsed = (new Parser('Александр Сергеевич Пушкин', $body, $imagesResponseData))->parse('bulma');
+
+        $expects = file_get_contents(__DIR__ . '/ParserTest/bulma.html');
+        $this->assertEquals($expects, $parsed);
+    }
 }
