84c82c6e6c8de87e390200fb5594735a20291b89
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2018-07-10 18:57:46 +0300

    Common class for formatters - wip.

--- src/Grabber/Formatter/Formatter.php
+++ src/Grabber/Formatter/Formatter.php
@@ -27,44 +27,4 @@ abstract class Formatter
     abstract public function tableOfContents();
 
     abstract public function section(Section $section);
-
-    protected function hasTableOfContents()
-    {
-        return $this->tocSections->isNotEmpty();
-    }
-
-    protected function tocLevels()
-    {
-        return $this->tocSections->map(function (Section $section) {
-            return $section->getLevel();
-        })->unique()->sort();
-    }
-
-    protected function sectionId($title)
-    {
-        return str_slug($title);
-    }
-
-    protected function sectionBody(Section $section)
-    {
-        return preg_replace('/(\s*<br.*?>\s*){3,}/m', '$1$1', nl2br($section->getBody()));
-    }
-
-    protected function toGallerySize($size)
-    {
-        return (int) ($size / 1.35);
-    }
-
-    protected function htmlBlock($open, Collection $items, $close)
-    {
-        $items = collect(array_map('trim', $items->toArray()))->filter();
-        if ($items->isEmpty()) {
-            return;
-        }
-
-        $open .= !empty($open) ? "\n" : '';
-        $close .= !empty($close) ? "\n" : '';
-
-        return "{$open}{$items->implode("\n")}\n{$close}\n";
-    }
 }
--- src/Grabber/Formatter/IlluminatedFormatter.php
+++ src/Grabber/Formatter/IlluminatedFormatter.php
@@ -210,4 +210,44 @@ class IlluminatedFormatter extends Formatter
 
         return "<div class='iwg-media video {$position}'>{$video}{$desc}</div>";
     }
+
+    protected function hasTableOfContents()
+    {
+        return $this->tocSections->isNotEmpty();
+    }
+
+    protected function tocLevels()
+    {
+        return $this->tocSections->map(function (Section $section) {
+            return $section->getLevel();
+        })->unique()->sort();
+    }
+
+    protected function sectionId($title)
+    {
+        return str_slug($title);
+    }
+
+    protected function sectionBody(Section $section)
+    {
+        return preg_replace('/(\s*<br.*?>\s*){3,}/m', '$1$1', nl2br($section->getBody()));
+    }
+
+    protected function toGallerySize($size)
+    {
+        return (int) ($size / 1.35);
+    }
+
+    protected function htmlBlock($open, Collection $items, $close)
+    {
+        $items = collect(array_map('trim', $items->toArray()))->filter();
+        if ($items->isEmpty()) {
+            return;
+        }
+
+        $open .= !empty($open) ? "\n" : '';
+        $close .= !empty($close) ? "\n" : '';
+
+        return "{$open}{$items->implode("\n")}\n{$close}\n";
+    }
 }
