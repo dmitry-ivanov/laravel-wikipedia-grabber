6cede6bf7f3ba162f545afef38b81b39111bcc55
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2018-07-10 17:16:56 +0300

    Bootstrap formatter - wip.

--- tests/WikipediaGrabber/Grabber/Parser/ParserTest.php
+++ tests/WikipediaGrabber/Grabber/Parser/ParserTest.php
@@ -38,4 +38,20 @@ class ParserTest extends TestCase
         $expects = file_get_contents(__DIR__ . '/ParserTest/bulma.html');
         $this->assertEquals($expects, $parsed);
     }
+
+    /** @test */
+    public function it_can_parse_body_for_bootstrap_format_also_excluding_an_empty_and_boring_sections()
+    {
+        $body = file_get_contents(__DIR__ . '/ParserTest/extract.txt');
+        $imagesResponseData = [
+            'wikitext' => file_get_contents(__DIR__ . '/ParserTest/wikitext.txt'),
+            'main_image' => require __DIR__ . '/ParserTest/main_image.php',
+            'images' => require __DIR__ . '/ParserTest/images.php',
+        ];
+
+        $parsed = (new Parser('Александр Сергеевич Пушкин', $body, $imagesResponseData))->parse('bootstrap');
+
+        $expects = file_get_contents(__DIR__ . '/ParserTest/bootstrap.html');
+        $this->assertEquals($expects, $parsed);
+    }
 }
