84771e14fa022e4d7a886d96cf717db151b6d4a5
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2018-07-10 15:35:09 +0300

    Bootstrap formatter - wip.

--- src/Grabber/Formatter/BootstrapFormatter.php
+++ src/Grabber/Formatter/BootstrapFormatter.php
@@ -32,18 +32,18 @@ class BootstrapFormatter extends Formatter
         //         ".iwg-gallery .iwg-media a {display:table-cell; width:{$galleryWidth}px; height:{$galleryHeight}px; vertical-align:middle}",
         //     ]));
         // }
-        //
-        // if ($this->hasMedia) {
-        //     $styles = $styles->merge(collect([
-        //         '.iwg-media {color:#757575; margin-bottom:1rem; padding:3px; box-shadow:0 .25rem .5rem 0 #BDBDBD; transition:.3s}',
-        //         '.iwg-media:hover {box-shadow:0 .5rem 1rem 0 #BDBDBD}',
-        //         '.iwg-media.left {float:left; clear:left; margin-right:1rem}',
-        //         '.iwg-media.right {float:right; clear:right; margin-left:1rem}',
-        //         '.iwg-media.audio, .iwg-media.video {width:275px; padding:5px}',
-        //         '.iwg-media audio, .iwg-media video {width:100%}',
-        //         '.iwg-media-desc {padding:.625rem 1rem; font-size:0.95rem; word-wrap:break-word}',
-        //     ]));
-        // }
+
+        if ($this->hasMedia) {
+            $styles = $styles->merge(collect([
+                '.iwg-media {color:#757575; margin-bottom:1rem; padding:3px; box-shadow:0 .25rem .5rem 0 #BDBDBD; transition:.3s}',
+                '.iwg-media:hover {box-shadow:0 .5rem 1rem 0 #BDBDBD}',
+                '.iwg-media.left {float:left; clear:left; margin-right:1rem}',
+                '.iwg-media.right {float:right; clear:right; margin-left:1rem}',
+                // '.iwg-media.audio, .iwg-media.video {width:275px; padding:5px}',
+                // '.iwg-media audio, .iwg-media video {width:100%}',
+                '.iwg-media-desc {padding:.625rem 1rem; font-size:0.95rem; word-wrap:break-word}',
+            ]));
+        }
 
         return $this->htmlBlock('<style>', $styles, '</style>');
     }
@@ -94,54 +94,54 @@ class BootstrapFormatter extends Formatter
 
     protected function images(Section $section)
     {
-        // if (!$section->hasImages()) {
-        //     return;
-        // }
-        //
-        // $images = $section->getImages()->map(function (Image $image) {
-        //     return $this->media($image);
-        // });
-        //
-        // return $this->htmlBlock(null, $images, null);
+        if (!$section->hasImages()) {
+            return;
+        }
+
+        $images = $section->getImages()->map(function (Image $image) {
+            return $this->media($image);
+        });
+
+        return $this->htmlBlock(null, $images, null);
     }
 
     protected function media(Image $image, $isGallery = false)
     {
-        // if ($image->isAudio()) {
-        //     return $this->audio($image, $isGallery);
-        // }
-        //
-        // if ($image->isVideo()) {
-        //     return $this->video($image, $isGallery);
-        // }
-        //
-        // return $this->image($image, $isGallery);
+        if ($image->isAudio()) {
+            return $this->audio($image, $isGallery);
+        }
+
+        if ($image->isVideo()) {
+            return $this->video($image, $isGallery);
+        }
+
+        return $this->image($image, $isGallery);
     }
 
     protected function image(Image $image, $isGallery = false)
     {
-        // $url = $image->getUrl();
-        // $alt = $image->getAlt();
-        // $width = $image->getWidth();
-        // $height = $image->getHeight();
-        // $position = $image->getPosition();
-        // $description = $image->getDescription();
-        // $originalUrl = $image->getOriginalUrl();
-        //
-        // if ($isGallery) {
-        //     $width = $this->toGallerySize($width);
-        //     $height = $this->toGallerySize($height);
-        // }
-        //
-        // $img = "<img src='{$url}' width='{$width}' height='{$height}' alt='{$alt}' />";
-        // $link = "<a href='{$originalUrl}' target='_blank'>{$img}</a>";
-        // $desc = !empty($description) ? "<div class='iwg-media-desc'>{$description}</div>" : '';
-        //
-        // if ($isGallery) {
-        //     return "<div class='iwg-media'>{$link}{$desc}</div>";
-        // }
-        //
-        // return "<div class='iwg-media {$position}' style='width:{$width}px'>{$link}{$desc}</div>";
+        $url = $image->getUrl();
+        $alt = $image->getAlt();
+        $width = $image->getWidth();
+        $height = $image->getHeight();
+        $position = $image->getPosition();
+        $description = $image->getDescription();
+        $originalUrl = $image->getOriginalUrl();
+
+        if ($isGallery) {
+            $width = $this->toGallerySize($width);
+            $height = $this->toGallerySize($height);
+        }
+
+        $img = "<img src='{$url}' width='{$width}' height='{$height}' alt='{$alt}' />";
+        $link = "<a href='{$originalUrl}' target='_blank'>{$img}</a>";
+        $desc = !empty($description) ? "<div class='iwg-media-desc'>{$description}</div>" : '';
+
+        if ($isGallery) {
+            return "<div class='iwg-media'>{$link}{$desc}</div>";
+        }
+
+        return "<div class='iwg-media {$position}' style='width:{$width}px'>{$link}{$desc}</div>";
     }
 
     protected function audio(Image $image, $isGallery = false)
